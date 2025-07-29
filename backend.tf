0d2c9845e6cd67e2e79087ca994b8f442c42c8b2
danielabouassalyy <abou0344@algonquinlive.com>
2025-07-29 01:04:13 -0400

    chore: pin Terraform CLI & azurerm provider versions

--- backend.tf
+++ backend.tf
@@ -1,8 +1,24 @@
 terraform {
+  # 1. Pin the Terraform CLI version
+  required_version = ">= 1.5.0"
+
+  # 2. Pin your AzureRM provider
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.37.0"
+    }
+  }
+
+  # 3. Your existing remote‐state backend
   backend "azurerm" {
     resource_group_name  = "cst8918-final-project-group-4"
     storage_account_name = "abou0344tfstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
+}
+
+provider "azurerm" {
+  features {}
 }
