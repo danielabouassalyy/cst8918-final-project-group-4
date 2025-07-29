174765d81718c8e5d60f59638200690eab490ebf
danielabouassalyy <abou0344@algonquinlive.com>
2025-07-29 02:31:16 -0400

    style: run terraform fmt

--- backend.tf
+++ backend.tf
@@ -12,11 +12,11 @@ terraform {
 
   # 3. Point at your new remote‐state backend in the CDTO subscription
   backend "azurerm" {
-    subscription_id       = "4f28dd96-a497-4f49-8d77-0c39fc199087"
-    resource_group_name   = "tfstate-rg"
-    storage_account_name  = "abou0344tfstatecdo"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+    subscription_id      = "4f28dd96-a497-4f49-8d77-0c39fc199087"
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "abou0344tfstatecdo"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
 
