eb477f4a33cc5fcf1d6777ed4fd3acca8546cf71
danielabouassalyy <abou0344@algonquinlive.com>
2025-07-29 00:08:44 -0400

    style: apply terraform fmt to backend.tf and modules

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "cst8918-final-project-group-4"
-    storage_account_name  = "abou0344tfstate"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
+    resource_group_name  = "cst8918-final-project-group-4"
+    storage_account_name = "abou0344tfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
