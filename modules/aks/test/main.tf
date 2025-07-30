8ae85dfacc74a666ac2fc802ed618d9f0c8e94ea
MichaelBalcerzak <balcerzak.michael@gmail.com>
2025-07-29 23:18:40 -0400

    fix the identification and remove aguments that cause error

--- modules/aks/test/main.tf
+++ modules/aks/test/main.tf
@@ -4,31 +4,23 @@ resource "azurerm_resource_group" "cluster" {
 }
 
 resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = var.aks_cluster_name
-  location            = azurerm_resource_group.cluster.location
-  resource_group_name = azurerm_resource_group.cluster.name
+  name                              = var.aks_cluster_name
+  location                          = azurerm_resource_group.cluster.location
+  resource_group_name               = azurerm_resource_group.cluster.name
   role_based_access_control_enabled = true
-  api_server_authorized_ip_ranges = ["203.0.113.0/24"]
-  dns_prefix          = "leaks1"
-  kubernetes_version  = 1.32
+  dns_prefix                        = "leaks1"
+  kubernetes_version                = 1.32
 
   default_node_pool {
-    name                = "default"
-    node_count          = 1
-    vm_size             = "Standard_B2s"
-    vnet_subnet_id      = var.subnet_id
-  }
-
-  addon_profile {
-    oms_agent {
-      enabled                    = true
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
-    }
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_B2s"
+    vnet_subnet_id = var.subnet_id
   }
 
   network_profile {
-    network_plugin    = "azure" # or "kubenet" depending on your setup
-    network_policy    = "azure" # or "calico"
+    network_plugin = "azure" # or "kubenet" depending on your setup
+    network_policy = "azure" # or "calico"
   }
 
   identity {
--- modules/redis/prod/main.tf
+++ modules/redis/prod/main.tf
@@ -11,9 +11,9 @@ resource "azurerm_redis_cache" "redis" {
   resource_group_name = var.resource_group_name
 
   # SKU options: Basic, Standard, Premium
-  sku_name = "Basic"
-  capacity = 1
-  family   = "C"
+  sku_name            = "Basic"
+  capacity            = 1
+  family              = "C"
   minimum_tls_version = "1.2"
   redis_configuration {
     maxmemory_policy = "allkeys-lru"
