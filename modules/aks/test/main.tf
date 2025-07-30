15f83fab2ffb45314fb3051ed3fc27c3958df738
MichaelBalcerzak <balcerzak.michael@gmail.com>
2025-07-30 01:16:10 -0400

    downgrade azurerm provider

--- modules/aks/test/main.tf
+++ modules/aks/test/main.tf
@@ -3,14 +3,6 @@ resource "azurerm_resource_group" "cluster" {
   location = var.location
 }
 
-resource "azurerm_log_analytics_workspace" "aks_logs" {
-  name                = "aks-logs"
-  location            = azurerm_resource_group.cluster.location
-  resource_group_name = azurerm_resource_group.cluster.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
 resource "azurerm_kubernetes_cluster" "cluster" {
   name                              = var.aks_cluster_name
   location                          = azurerm_resource_group.cluster.location
@@ -37,54 +29,5 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
   tags = {
     Environment = "test"
-  }
-
-  api_server_access_profile {
-    authorized_ip_ranges = [
-      "10.1.0.0/16" # Replace with your actual IP
-    ]
-  }
-}
-
-resource "azurerm_monitor_diagnostic_setting" "aks_diagnostics" {
-  name                       = "aks-diagnostics"
-  target_resource_id         = azurerm_kubernetes_cluster.cluster.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.aks_logs.id
-
-  enabled_log {
-    category = "kube-apiserver"
-
-    retention_policy {
-      enabled = false
-      days    = 0
-    }
-  }
-
-  enabled_log {
-    category = "kube-controller-manager"
-
-    retention_policy {
-      enabled = false
-      days    = 0
-    }
-  }
-
-  enabled_log {
-    category = "cluster-autoscaler"
-
-    retention_policy {
-      enabled = false
-      days    = 0
-    }
-  }
-
-  metric {
-    category = "AllMetrics"
-    enabled  = true
-
-    retention_policy {
-      enabled = false
-      days    = 0
-    }
   }
 }
