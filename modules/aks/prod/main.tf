b3608fda857ee8e0f2b5d464e52bbddfa01669c5
danielabouassalyy <abou0344@algonquinlive.com>
2025-08-09 12:58:40 -0400

    Fix AKS prod: enable autoscaling + set non-overlapping service CIDR

--- modules/aks/prod/main.tf
+++ modules/aks/prod/main.tf
@@ -1,46 +1,3 @@
-# Log Analytics Workspace
-resource "azurerm_log_analytics_workspace" "aks_logs" {
-  name                = "${var.aks_cluster_name}-logs"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
-# AKS Prod Cluster
-resource "azurerm_kubernetes_cluster" "cluster" {
-  dns_prefix          = var.aks_cluster_name
-  name                = var.aks_cluster_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  kubernetes_version  = "1.32"
-
-  default_node_pool {
-    name                 = "default"
-    auto_scaling_enabled = true
-    min_count            = 1
-    max_count            = 3
-    type                 = "VirtualMachineScaleSets"
-    vm_size              = "Standard_B2s"
-    vnet_subnet_id       = var.subnet_id
-  }
-
-  network_profile {
-    network_plugin    = "azure"
-    network_policy    = "azure"
-    load_balancer_sku = "standard"
-    outbound_type     = "loadBalancer"
-
-    # Non-overlapping with VNet/subnets
-    service_cidr   = "172.16.0.0/16"
-    dns_service_ip = "172.16.0.10"
-  }
-
-  identity { type = "SystemAssigned" }
-
-  tags = { Environment = "Production" }
-}
-
 # Diagnostic settings for OMS (Log Analytics)
 resource "azurerm_monitor_diagnostic_setting" "aks_diagnostics" {
   name                       = "aks-diagnostics"
@@ -49,22 +6,34 @@ resource "azurerm_monitor_diagnostic_setting" "aks_diagnostics" {
 
   enabled_log {
     category = "kube-apiserver"
-    retention_policy { enabled = false, days = 0 }
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
   }
 
   enabled_log {
     category = "kube-controller-manager"
-    retention_policy { enabled = false, days = 0 }
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
   }
 
   enabled_log {
     category = "cluster-autoscaler"
-    retention_policy { enabled = false, days = 0 }
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
   }
 
   metric {
     category = "AllMetrics"
     enabled  = true
-    retention_policy { enabled = false, days = 0 }
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
   }
 }
