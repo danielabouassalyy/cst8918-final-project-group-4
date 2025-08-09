5a97d5e60ab1c8ca8444b2fa1e2ec23d6cc76624
danielabouassalyy <abou0344@algonquinlive.com>
2025-08-09 13:15:01 -0400

    fix(ci): restore AKS prod resources referenced by diagnostics

--- modules/aks/prod/main.tf
+++ modules/aks/prod/main.tf
@@ -1,3 +1,46 @@
+# Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "aks_logs" {
+  name                = "${var.aks_cluster_name}-logs"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# AKS Prod Cluster
+resource "azurerm_kubernetes_cluster" "cluster" {
+  dns_prefix          = var.aks_cluster_name
+  name                = var.aks_cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  kubernetes_version  = "1.32"
+
+  default_node_pool {
+    name                 = "default"
+    auto_scaling_enabled = true
+    min_count            = 1
+    max_count            = 3
+    type                 = "VirtualMachineScaleSets"
+    vm_size              = "Standard_B2s"
+    vnet_subnet_id       = var.subnet_id
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "azure"
+    load_balancer_sku = "standard"
+    outbound_type     = "loadBalancer"
+
+    # Non-overlapping with VNet/subnets
+    service_cidr   = "172.16.0.0/16"
+    dns_service_ip = "172.16.0.10"
+  }
+
+  identity { type = "SystemAssigned" }
+
+  tags = { Environment = "Production" }
+}
+
 # Diagnostic settings for OMS (Log Analytics)
 resource "azurerm_monitor_diagnostic_setting" "aks_diagnostics" {
   name                       = "aks-diagnostics"
